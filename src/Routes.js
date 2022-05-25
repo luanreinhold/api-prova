0a7dfb468984b8ea5ea921aa8098c01fee40b5d2
JVMDS <jvmdsoficial24@gmail.com>
2022-05-25 09:38:02 -0300

    Api react para vc

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, routes , route } from "react-router-dom";
+
+import Home from './pages/home/index.jsx'
+import corPrimaria from './pages/corPrimaria/index.jsx'
+import Frequencia from './pages/Frequencia/index.jsx'
+import Ingresso from './pages/Ingresso'
+import MaiorNumero from './pages/MaiorNumero'
+
+
+export default function Routes() {
+    <BrowserRouter>
+        <routes>
+            <route>
+                
+            </route>
+        </routes>
+    </BrowserRouter>
+}
+
+// Não sei pq ta tendo esses problemas se conseguir arrumar vai ser bom
